62727d205aa8cf23d897e5d97c179d16ed016617
Bryan Lam <blam.gml@gmail.com>
2020-05-23 16:24:07 -0600

    Added test for CreatingThreads

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Thread;
+use App\User;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function guests_may_not_create_threads()
+    {
+        $this->expectException(AuthenticationException::class);
+
+        $thread = factory(Thread::class)->make();
+
+        $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $thread = factory(Thread::class)->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+
+}
