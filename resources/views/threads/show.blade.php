d4ba2c5e2389c1fd4b7cf225516ae92ec740e4cb
Bryan Lam <blam.gml@gmail.com>
2020-05-23 15:01:21 -0600

    Added user name to forum post. Add scaffol tet for ParticipateInForm

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ $thread->title }}</div>
+                <div class="card-header">
+                    <a href="#">{{ $thread->creator->name }}</a> posted:
+                    {{ $thread->title }}
+                </div>
 
                 <div class="card-body">
                     {{ $thread->body }}
