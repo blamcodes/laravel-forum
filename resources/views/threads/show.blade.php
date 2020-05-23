d824289ae3eda12becf5ebfe54f18ffa09f28e57
Bryan Lam <blam.gml@gmail.com>
2020-05-23 15:49:56 -0600

    Added reply form for authenticated users along with message for unauthenticated users

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,23 @@
             @endforeach
         </div>
     </div>
+
+    @if (auth()->check())
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <textarea class="form-control" name="body" id="body" rows="3" placeholder="Have something to say?" rows="5"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Post</button>
+                </form>
+            </div>
+        </div>
+    @else
+        <p class="text-center">Please <a href="{{ route('login') }}">sign</a> in to participate in this discussion.</p>
+    @endif
 </div>
 @endsection
