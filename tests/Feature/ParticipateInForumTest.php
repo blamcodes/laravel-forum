56f2e76f72f45b3e7ee297b83ee417d0cdfc2dca
Bryan Lam <blam.gml@gmail.com>
2020-05-23 15:30:50 -0600

    Added tests to make check for authenticated access

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -5,13 +5,22 @@ namespace Tests\Feature;
 use App\Reply;
 use App\Thread;
 use App\User;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ParticipateInForum extends TestCase
+class ParticipateInForumTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException(AuthenticationException::class);
+
+        $this->post('/threads/1/replies', []);
+    }
 
     /** @test */
     function an_authenticated_user_may_participate_in_forum_threads()
@@ -23,8 +32,8 @@ class ParticipateInForum extends TestCase
         $thread = factory(Thread::class)->create();
 
         // When the user adds a reply to the thread
-        $reply = factory(Reply::class)->create();
-        $this->post('/threads/' . $thread->id . '/replies', $reply->toArray());
+        $reply = factory(Reply::class)->make();
+        $this->post($thread->path() . '/replies', $reply->toArray());
 
         //Then their reply should be visible on the page.
         $this->get($thread->path())
