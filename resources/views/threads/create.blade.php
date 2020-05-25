2ae81b95231db5eed68006547b3f64dbf2c8d55a
Bryan Lam <blam.gml@gmail.com>
2020-05-25 08:38:08 -0600

    Updated create view for Thread to include validation error output and dropdown for selecting Channels

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -12,13 +12,24 @@
                         {{ csrf_field() }}
 
                         <div class="form-group">
+                          <label for="channel_id">Choose a Channel:</label>
+                          <select class="form-control" name="channel_id" id="channel_id" required>
+                            <option value="">Choose One ...</option>
+
+                            @foreach (App\Channel::all() as $channel)
+                                <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : ''}}>{{ $channel->name }}</option>
+                            @endforeach
+                          </select>
+                        </div>
+
+                        <div class="form-group">
                           <label for="title">Title:</label>
-                          <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}">
+                          <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}" required>
                         </div>
 
                         <div class="form-group">
                           <label for="body">Body:</label>
-                          <textarea class="form-control" name="body" id="body" rows="8">{{ old('body') }}</textarea>
+                          <textarea class="form-control" name="body" id="body" rows="8" required>{{ old('body') }}</textarea>
                         </div>
 
                         <div class="form-group">
