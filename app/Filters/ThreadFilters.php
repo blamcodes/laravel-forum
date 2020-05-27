668416b960aeb3c5ff2439a8086f6f1680fa4868
Bryan Lam <blam.gml@gmail.com>
2020-05-27 08:50:15 -0600

    Refactoring by creating an abstract filters class that can be reused. Extracting out filters method

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -5,33 +5,9 @@ namespace App\Filters;
 use App\User;
 use Illuminate\Http\Request;
 
-class ThreadFilters
+class ThreadFilters extends Filters
 {
-    /**
-     * @var Request
-     */
-    protected $request;
-
-    protected $builder;
-
-    /**
-     * ThreadFilters constructor.
-     */
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
-    public function apply($builder)
-    {
-        $this->builder = $builder;
-
-        if ($this->request->has('by')) {
-            $this->by($this->request->by);
-        }
-
-        return $this->builder;
-    }
+    protected $filters = ['by'];
 
     protected function by($username)
     {
