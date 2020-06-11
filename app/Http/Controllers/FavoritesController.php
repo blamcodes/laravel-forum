43922f3f7cd45d88a7db8b47efa48c8958ec20b2
Bryan Lam <blam.gml@gmail.com>
2020-06-10 21:49:59 -0600

    Added FavoritesController

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Favorite;
+use App\Reply;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class FavoritesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Reply $reply)
+    {
+        return $reply->favorite();
+    }
+}
