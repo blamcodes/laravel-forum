beca50b04f9a5eaa43dbccd5497907e27b2821aa
Bryan Lam <blam.gml@gmail.com>
2020-06-10 21:51:45 -0600

    Added favorites polymorphic relationship and favorite method

--- app/Reply.php
+++ app/Reply.php
@@ -12,4 +12,18 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if (! $this->favorites()->where($attributes)->exists()) {
+            $this->favorites()->create($attributes);
+        }
+    }
 }
